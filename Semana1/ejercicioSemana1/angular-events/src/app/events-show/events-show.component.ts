6fc65e65f01f7f58a851c8e3a64a1d603b5d8778
Joaquin Vicente Alonso Saiz <joaalsai@ieslavereda.es>
2018-05-05 22:56:04 +0200

    Semana 1

---
+++ Semana1/ejercicioSemana1/angular-events/src/app/events-show/events-show.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { IEvent } from 'interfaces/i-event';
+
+@Component({
+  selector: 'app-events-show',
+  templateUrl: './events-show.component.html',
+  styleUrls: ['./events-show.component.css']
+})
+export class EventsShowComponent implements OnInit {
+
+  eventos: IEvent[] = [{
+    title: 'Primer evento',
+    image: '',
+    date: new Date('2018-05-05').toLocaleDateString(),
+    description: 'Descripción del primer evento',
+    price: 1
+  },
+  {
+    title: 'Segunto evento',
+    image: '',
+    date: new Date('2018-05-05').toLocaleDateString(),
+    description: 'Descripción del segundo evento',
+    price: 2
+  }];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
